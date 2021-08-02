413bc49f8843af02a96ddc40e2d05753a4f0cc3b
danielbossier <dbossier21@gmail.com>
2021-08-01 22:58:11 -0700

    initial commit plus basic animation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
+    <link rel="stylesheet" href="bower_components/chartist/dist/chartist.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <h1 class="animate__animated animate__heartBeat">Animation!</h1>
+    <h2 class="animate__animated animate__fadeIn">Dat Jello dough.</h2>
+
+    <div class="ct-chart ct-perfect-fourth"></div>
+
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<style>
+.animate__animated.animate__heartBeat {
+  --animate-duration: 5s;
+}
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
+.animate__animated.animate__fadeIn {
+  --animate-duration: 5s;
+}
+</style>
